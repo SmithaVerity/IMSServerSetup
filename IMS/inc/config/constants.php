c3ab91717644a7d42c64a631f957ab87c048e824
Smitha Menon <98273657+SmithaVerity@users.noreply.github.com>
2022-11-13 18:20:37 +0530

    Update constants.php

--- IMS/inc/config/constants.php
+++ IMS/inc/config/constants.php
@@ -5,16 +5,16 @@
 	
 	// Database parameters
 	// Data source name
-	define('DSN', 'mysql:host=localhost;dbname=shop_inventory');
+	define('DSN', 'mysql:host=cloudtestingdb.crgya7bbq8o7.ap-southeast-1.rds.amazonaws.com;dbname=shop_inventory');
 	
 	// Hostname
-	define('DB_HOST', 'localhost');
+	define('DB_HOST', 'cloudtestingdb.crgya7bbq8o7.ap-southeast-1.rds.amazonaws.com');
 	
 	// DB user
-	define('DB_USER', 'ubuntu');
+	define('DB_USER', 'admin');
 	
 	// DB password
-	define('DB_PASSWORD', 'Ubuntu@123');
+	define('DB_PASSWORD', 'admin123');
 	
 	// DB name
 	define('DB_NAME', 'shop_inventory');
