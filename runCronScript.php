1c435fa2c7d310fe0f9ed611f6d973f70019aed7
Smitha Menon <98273657+SmithaVerity@users.noreply.github.com>
2023-03-19 01:31:01 +0530

    Update runCronScript.php

--- runCronScript.php
+++ runCronScript.php
@@ -1,6 +1,6 @@
 <?php
-	require_once('IMS/inc/config/constants.php');
-	require_once('IMS/inc/config/db.php');
+	require_once('var/www/html/IMS/inc/config/constants.php');
+	require_once('var/www/html/IMS/inc/config/db.php');
 
 			// Start the insert process
 			$sql = 'INSERT INTO cron_table(`number`, `dt`) VALUES('12345', now());';
